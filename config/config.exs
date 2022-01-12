9511c56fe81f66fe63fd2113222c81b18aba65f8
Dairon Medina Caro <dairon@system76.com>
2022-01-12 10:32:19 -0500

    Simplify config to more recent elixir versions style

--- config/config.exs
+++ config/config.exs
@@ -1,38 +1,22 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-use Mix.Config
+import Config
 
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for
-# 3rd-party users, it should be done in your "mix.exs" file.
+# Configures Redis connection
+# For a full list of configuration options, please see:
+# https://hexdocs.pm/redix/Redix.html#start_link/1-connection-options
+config :beebee,
+       :storage_backend,
+       {BeeBee.ShortUrls.Redis,
+        [
+          host: System.get_env("REDIS_HOST", "127.0.0.1"),
+          port: System.get_env("REDIS_PORT", "6379") |> String.to_integer(),
+          password: System.get_env("REDIS_PASSWORD", ""),
+          database: 0
+        ]}
 
-# You can configure for your application as:
-#
-#     config :beebee, key: :value
-#
-# And access this configuration in your application as:
-#
-#     Application.get_env(:beebee, :key)
-#
-# Or configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
+# Configures Elixir's Logger
+config :logger,
+  backends: [LoggerJSON]
 
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
-
-config :exredis,
-  host: System.get_env("REDIS_HOST") || "127.0.0.1",
-  port: 6379,
-  password: "",
-  db: 0,
-  reconnect: :no_reconnect,
-  max_queue: :infinity
+config :logger_json, :backend,
+  formatter: LoggerJSON.Formatters.DatadogLogger,
+  metadata: :all
