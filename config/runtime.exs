3926fefd3ba2b3aa8c6eaffc1ff134c863df7e57
Dairon Medina Caro <dairon@system76.com>
2022-03-14 22:41:20 -0400

    Load config from secrets

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,5 +1,10 @@
 import Config
 
+config_secrets =
+  "CONFIG"
+  |> System.get_env("{}")
+  |> Jason.decode!()
+
 # Configures Redis connection
 # For a full list of configuration options, please see:
 # https://hexdocs.pm/redix/Redix.html#start_link/1-connection-options
@@ -7,9 +12,9 @@ config :beebee,
        :storage_backend,
        {BeeBee.Storage.Redis,
         [
-          host: System.get_env("REDIS_HOST", "127.0.0.1"),
-          port: System.get_env("REDIS_PORT", "6379") |> String.to_integer(),
-          password: System.get_env("REDIS_PASSWORD", ""),
+          host: Access.get(config_secrets, "REDIS_HOST", "127.0.0.1"),
+          port: Access.get(config_secrets, "REDIS_PORT", "6379") |> String.to_integer(),
+          password: Access.get(config_secrets, "REDIS_PASSWORD", ""),
           database: 0
         ]}
 
