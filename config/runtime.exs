e48eb6960c9bd235e70117ac68c406e84e1277ba
Dairon Medina Caro <dairon@system76.com>
2022-03-15 12:27:38 -0400

    Change config and deploy to old existing infrastructure

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,10 +1,5 @@
 import Config
 
-config_secrets =
-  "CONFIG"
-  |> System.get_env("{}")
-  |> Jason.decode!()
-
 # Configures Redis connection
 # For a full list of configuration options, please see:
 # https://hexdocs.pm/redix/Redix.html#start_link/1-connection-options
@@ -12,9 +7,9 @@ config :beebee,
        :storage_backend,
        {BeeBee.Storage.Redis,
         [
-          host: Access.get(config_secrets, "REDIS_HOST", "127.0.0.1"),
-          port: Access.get(config_secrets, "REDIS_PORT", "6379") |> String.to_integer(),
-          password: Access.get(config_secrets, "REDIS_PASSWORD", ""),
+          host: System.get_env("REDIS_HOST", "127.0.0.1"),
+          port: System.get_env("REDIS_PORT", "6379") |> String.to_integer(),
+          password: System.get_env("REDIS_PASSWORD", ""),
           database: 0
         ]}
 
