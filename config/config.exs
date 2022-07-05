f7710471ee5c806c0b5b79568e757d6a81fb4f7a
Mike Cifelli <26522946+macifell@users.noreply.github.com>
2022-07-05 19:35:27 -0400

    Add optional basic auth (#12)

--- config/config.exs
+++ config/config.exs
@@ -1,18 +1,5 @@
 import Config
 
-# Configures Redis connection
-# For a full list of configuration options, please see:
-# https://hexdocs.pm/redix/Redix.html#start_link/1-connection-options
-config :beebee,
-       :storage_backend,
-       {BeeBee.Storage.Redis,
-        [
-          host: System.get_env("REDIS_HOST", "127.0.0.1"),
-          port: System.get_env("REDIS_PORT", "6379") |> String.to_integer(),
-          password: System.get_env("REDIS_PASSWORD", ""),
-          database: 0
-        ]}
-
 # Configures Elixir's Logger
 config :logger,
   backends: [LoggerJSON]
